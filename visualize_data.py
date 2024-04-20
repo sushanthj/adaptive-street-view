df8cfa3370dda0269bb28ac3acf877508ff002d1
rcarbonn <ronit.hire@gmail.com>
2024-04-20 16:49:22 +0000

    visualization changes

--- visualize_data.py
+++ visualize_data.py
@@ -74,9 +74,10 @@ def visualize_a_sample(sample, map_lidar):
     img_to_show = np.zeros((img_size, img_size*2, 3))
     img_to_show[:, 0:img_size] = (img_gt.numpy()+1)/2
     img_to_show[:, img_size:] = img_depth_lidar_inv.reshape(img_size, img_size, 3)
-    cv2.imshow('RGB and depth GT', img_to_show[:, :, [2, 1, 0]])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('RGB and depth GT', img_to_show[:, :, [2, 1, 0]])
+    cv2.imwrite('rgb_and_depth_gt.png', img_to_show[:, :, [2, 1, 0]])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 def visualize_cam_pose_on_map(dataloader_val, dataloader_train, map_lidar):
@@ -95,7 +96,7 @@ def visualize_cam_pose_on_map(dataloader_val, dataloader_train, map_lidar):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_id',  type=str, required=True)
+    parser.add_argument('--seq_id',  type=str, default='seq1')
     parser.add_argument('--name_data', choices=['clean', 'noisy'], type=str, required=True)
 
     args = parser.parse_args()
@@ -105,7 +106,12 @@ if __name__ == '__main__':
     cfg.read('configs/config.ini')
     cfg = cfg['DEFAULT']
     print(cfg)
-    cfg['log_id'] = str(args.log_id)
+
+    data_cfg = configparser.ConfigParser()
+    data_cfg.read('configs/data_ids.ini')
+    data_cfg = data_cfg['DEFAULT']
+    print(data_cfg)
+    cfg['log_id'] = data_cfg[args.seq_id]
 
     dataloader_train = get_data_loader(cfg, args.name_data, 'train')
     dataloader_val = get_data_loader(cfg, args.name_data, 'val')
@@ -115,7 +121,8 @@ if __name__ == '__main__':
 
     # to visualize the graph, LiDAR depth, ground truth image for one single sample
     sample_ind_to_visualize = 7
+    pv.start_xvfb()
     visualize_a_sample(dataloader_val.dataset[sample_ind_to_visualize], map_lidar)
 
     # to visualize the camera poses on the map of the whole dataset
-    visualize_cam_pose_on_map(dataloader_val, dataloader_train, map_lidar)
+    # visualize_cam_pose_on_map(dataloader_val, dataloader_train, map_lidar)
